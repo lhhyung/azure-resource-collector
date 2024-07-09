3822ab9ce37fe6c7ee9e7c4048a1f4ef62552be4
lhhyung <lhhyung511@mz.co.kr>
2024-07-09 13:39:56 +0900

    feat: Modify output format in disks, virtual_machines manager.py

--- src/service/disks/disks_manager.py
+++ src/service/disks/disks_manager.py
@@ -6,10 +6,21 @@ def collect_disks():
     disks_list = disks_connector.get_disks_list()
 
     column_width = 40
+
     print("disks_list")
-    print("-" * (column_width * 2))
+    print("Name".ljust(column_width) + "Storage Type".ljust(column_width) + "Size(GB)".ljust(
+        column_width) + "Owner".ljust(column_width) + "Resource Group".ljust(column_width) + "Location".ljust(
+        column_width))
+    print("-" * (column_width * 6))
 
     for disk in disks_list:
-        print(disk)
+        name = disk.name[:column_width].ljust(column_width)
+        storage_type = disk.sku.name.ljust(column_width)
+        size_gb = str(disk.disk_size_gb).ljust(column_width)
+        owner = disk.managed_by.split('/')[-1].ljust(column_width)
+        resource_group = disk.id.split('/')[4].ljust(column_width)
+        location = disk.location.ljust(column_width)
+
+        print(name + storage_type + size_gb + owner + resource_group + location)
 
     print()
--- src/service/virtual_machines/virtual_machines_manager.py
+++ src/service/virtual_machines/virtual_machines_manager.py
@@ -6,10 +6,19 @@ def collect_virtual_machines():
     virtual_machines_list = virtual_machines_connector.get_virtual_machines_list()
 
     column_width = 40
-    print("virtual_machines_list")
-    print("-" * (column_width * 2))
 
-    for virtual_machine in virtual_machines_list:
-        print(virtual_machine)
+    print("virtual_machines_list")
+    print("Name".ljust(column_width) + "Subscription".ljust(column_width) + "Resource Group".ljust(column_width) + "Location".ljust(column_width))
+    print("-" * (column_width * 4))
+
+    for vm in virtual_machines_list:
+        vm_name = vm.name
+        subscription_id = vm.id.split('/')[2]  # Extract subscription ID from ID
+        resource_group = vm.id.split('/')[4]  # Extract resource group from ID
+        location = vm.location
+
+        print(
+            f"{vm_name:<{column_width}}{subscription_id:<{column_width}}{resource_group:<{column_width}}{location:<{column_width}}")
 
     print()
+
